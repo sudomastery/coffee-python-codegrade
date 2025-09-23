d44b4fd844b5c1d97e8805c950fae35faf8bbff6
sudomastery <roykoigu@protonmail.com>
2025-09-23 04:45:19 +0300

    Add Customer class with name validation and internal storage

---
+++ customer.py
@@ -0,0 +1,30 @@
+class Customer:
+    #delcare internal variable
+    _everything = []
+
+    def __init__(self,name):
+        self.name = name
+        Customer._everything.append(self) #add instance to internal variable
+
+
+    #treat method as attribute
+    @property
+    def name (self):
+        return self.name
+    
+    #set value of property
+    
+    @name.setter
+    def name(self, value):
+
+
+
+        #check if string lenght is between 1 and 15 characters
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string")
+        if not (1 <= len(value) <=15):
+            raise ValueError("Name must be between 1 and 15 characters")
+        self._name = value
+
+
+        #to declare other functions
